d0e6058d118af0d40aec7ec2b8368e6b45258103
Shokkobarokko <avetis.01@mail.ru>
2026-06-11 22:48:34 +0300

    Initial Commit

---
+++ Signal/SignalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalApp.swift
+//  Signal
+//
+//  Created by Аветис Парсаданян on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SignalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
